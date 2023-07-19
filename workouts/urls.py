6115c2ffa8c6d4b907840f18b2a9f9232dbde1cb
StudioCrew-design <dev.studiocrew@gmail.com>
2023-07-19 11:56:16 +0530

    Added template inheritance and include includes/partials/shared html file to inject navbar

--- workouts/urls.py
+++ workouts/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:day>", views.workout_by_day_in_number),
     path("<str:day>", views.workout, name="day-workout")
 ]
